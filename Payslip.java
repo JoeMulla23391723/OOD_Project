8509047863ee930d40405845313f1782d4c037c6
JoeMulla23391723 <23391723@studentmail.ul.ie>
2024-11-27 22:52:45 +0000

    payslip format change

--- Payslip.java
+++ Payslip.java
@@ -16,23 +16,33 @@ public class Payslip {
         FullTimeDeductionsCalculator fullTimeCalculator = new FullTimeDeductionsCalculator();
         if (employee instanceof PartTimeEmployee) { //part-time payslips only show current months based on one hoursWorked value
             PartTimeEmployee partTimeEmployee = (PartTimeEmployee) employee;
-            System.out.println("Date" + "25/" + month + "/2024");
-            System.out.println("Name" + partTimeEmployee.getName());
-            System.out.println("Employee ID: " + partTimeEmployee.getId());
-            System.out.println("Hours worked this period: " + partTimeEmployee.getHoursWorkedThisPayPeriod());
-            System.out.println("Gross Pay: " + (partTimeEmployee.getHoursWorkedThisPayPeriod() * partTimeEmployee.getHourlyRate()));
-            System.out.println("USC: " + partTimeCalculator.calculateUscPaid(employeeID));
-            System.out.println("PRSI: " + partTimeCalculator.calculateUscPaid(employeeID));
-            System.out.println("Union Fees: " + partTimeCalculator.calculateUnionFees(employeeID));
-            System.out.println("Health Insurance: " + partTimeCalculator.calculateHeathInsuranceCost(employeeID));
-            System.out.println("Net Tax: " + partTimeCalculator.calculateNetTax(employeeID));
-            System.out.println("Net Pay: " + partTimeCalculator.calculateNetPay(employeeID));
-            System.out.println("Prsi Subclass: " + partTimeCalculator.getPrsiSubClass());
+
+            System.out.println("--------------------------------------------------------");
+            System.out.println("                     Payslip                           ");
+            System.out.println("--------------------------------------------------------");
+            System.out.println("Employee Name: " + partTimeEmployee.getName());
+            System.out.println("Employee ID:   " + partTimeEmployee.getId());
+            System.out.println("Date:          " + "25/" + month + "/2024");
+            System.out.println("--------------------------------------------------------");
+            System.out.println("Hours Worked:        " + partTimeEmployee.getHoursWorkedThisPayPeriod());
+            System.out.println("Gross Pay:           €" + (partTimeEmployee.getHoursWorkedThisPayPeriod() * partTimeEmployee.getHourlyRate()));
+            System.out.println("--------------------------------------------------------");
+            System.out.println("Deductions:");
+            System.out.println("    USC:             €" + partTimeCalculator.calculateUscPaid(employeeID));
+            System.out.println("    PRSI:            €" + partTimeCalculator.calculatePrsiPaid(employeeID));
+            System.out.println("    Union Fees:      €" + partTimeCalculator.calculateUnionFees(employeeID));
+            System.out.println("    Health Insurance:€" + partTimeCalculator.calculateHeathInsuranceCost(employeeID));
+            System.out.println("    Net Tax:         €" + partTimeCalculator.calculateNetTax(employeeID));
+            System.out.println("--------------------------------------------------------");
+            System.out.println("PRSI Subclass:       " + partTimeCalculator.getPrsiSubClass());
+            System.out.println("Net Pay:            €" + partTimeCalculator.calculateNetPay(employeeID));
+            System.out.println("--------------------------------------------------------");
+
         }
 
         else if (employee instanceof FullTimeEmployee) { //Creates a full-time payslip object that is then stored in an arraylist
             payslipArrayList = new ArrayList<>();
-            for (int i = 11; i <= 6; i++) {
+            for (int i = 6; i <= 11; i++) {
                 FullTimeEmployee fullTimeEmployee = (FullTimeEmployee) employee;
                 fullTimeEmployee.setSalary(employeeID);
                 PayslipObjects payslip = new PayslipObjects(
@@ -44,7 +54,7 @@ public class Payslip {
                         fullTimeCalculator.calculateUnionFees(employeeID),
                         fullTimeCalculator.calculateHeathInsurance(employeeID),
                         fullTimeCalculator.calculateNetTax(employeeID),
-                        Employees.calculateFullTimeNetPay(employeeID),
+                        fullTimeCalculator.calculateFullTimeNetPay(employeeID),
                         fullTimeCalculator.getPrsiSubClass()
                 );
                 payslipArrayList.add(payslip); //can later bring up a specific payslip, doesn't show it on method call.
