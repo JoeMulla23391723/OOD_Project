238ba799810a9baa013ed1c774e707344dbcd9f3
mcmonaglejack <21325855@studentmail.ul.ie>
2024-11-27 22:27:23 +0000

    Update FullTimeEmployee.java

--- FullTimeEmployee.java
+++ FullTimeEmployee.java
@@ -1,10 +1,9 @@
-
 public class FullTimeEmployee extends Employee {
     //subclass of employee for a full-time employee
     private String salaryScalePoint; //int to represent the point of the salary scale the employee is at
     private String department;// department name of the employee
-    private int salary;//yearly salary of the employee
-    private String professionalCategory;//Employee's professional category eg (Academic/Administrative/IT/Technical etc...)
+    private double salary;//yearly salary of the employee//
+
 
 
     public FullTimeEmployee(String[] employeeDetails) {
@@ -12,7 +11,7 @@ public class FullTimeEmployee extends Employee {
         this.salaryScalePoint = employeeDetails[17];
         this.department = employeeDetails[18];
         this.salary = Integer.parseInt(employeeDetails[19]);
-        this.professionalCategory = employeeDetails[20];
+
     }
 
     public FullTimeEmployee(String name, int id, String jobTitle, String pps, String email, String address, String phoneNum, int age, boolean medicalCard, boolean healthInsurance, String status, String[] taxCredits, String password, int numIncomes, boolean higherEarner, String[] unions, String healthPlan, String healthPlanType, String salaryScalePoint, String department, int salary) {
@@ -37,23 +36,20 @@ public class FullTimeEmployee extends Employee {
         return department;
     }
 
-
-
     public void setDepartment(String department) {
         this.department = department;
     }
 
-    // Getter and Setter for salary
-    @Override
+
+    public void setSalary(int employeeId) {
+    FullTimeDeductionsCalculator salaryCalc = new FullTimeDeductionsCalculator();
+    this.salary = salaryCalc.getEmployeeSalaryWithoutDeductions(employeeId);
+    }
+
+    // Getter and Setter for professional category
     public double getSalary() {
         return salary;
     }
 
-    public void setSalary(int salary) {
-        this.salary = salary;
-    }
-
-
-    
 
 }
