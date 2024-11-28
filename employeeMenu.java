14046f6adb7f0ff661dd6f7531ef132055300ca1
rmcguire211 <rebeccamcg13@gmail.com>
2024-11-28 21:02:12 +0000

    Update employeeMenu.java

--- employeeMenu.java
+++ employeeMenu.java
@@ -1,23 +1,2 @@
 
-import java.util.Scanner;
 
-public class employeeMenu {
-
-	public static void showMenu() {
-		Scanner in = new Scanner(System.in);
-				System.out.print("Select an option: ");
-				System.out.print("View C)urrent Payslip or View P)ast Payslips ");
-				String command = in.nextLine().toUpperCase();
-				
-				if(command.equals("C")) {
-					System.out.print("Here is your payslip: ");
-				}else if (command.equals("P")) {
-					System.out.print("Here is your past payslips: ");
-				}else {
-					System.out.print("Option is incorrect. Logging you out.");
-				
-				}
-		
-	}
- 
-}
