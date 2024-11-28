4de9b15c87c3414d6e28b8f6a35cda475f0e9f30
rmcguire211 <rebeccamcg13@gmail.com>
2024-11-28 22:01:38 +0000

    Update EmployeeMenu.java

--- EmployeeMenu.java
+++ EmployeeMenu.java
@@ -1,31 +1,59 @@
+package OOD;
+
 import java.util.Scanner;
 
 public class EmployeeMenu {
 	public static void showMenu(int employeeID) {
 		Scanner in = new Scanner(System.in);
-		System.out.print("Select option: ");
-		System.out.println("C)urrent Payslip, P)ast payslips , G)et Index of Employee L)ogout "); 
-		String command = in.nextLine().toUpperCase(); 
+		System.out.println("Select option applies to you: ");
+		System.out.println("I am a F)ull-time employye\nI am as P)art-time employee L)ogout "); 
+		String chosen = in.nextLine().toUpperCase(); 
 		
-		Payslip payslip = new Payslip(); 
-		
-		if(command.equals("C")) {
+		if(chosen.equals("F")) {
+			System.out.println("Select option applies to you: ");
+			System.out.println("C)urrent Payslip\n P)ast payslips L)ogout "); 
+			String command = in.nextLine().toUpperCase(); 
 			
-			payslip.printPayslip(employeeID);
+			Payslip payslip = new Payslip(); 
 			
-		}else if(command.equals("P")) {
-			System.out.print("Here are your past payslips"); 
-			 
-		}else if(command.equals("G")) {
-			System.out.print(Employees.getIndexOfEmployeeID(employeeID)); 
-		}
-		
-		else {
-			
-			System.out.print("Logging out...");
+			if(command.equals("C")) {
+				System.out.print("Here is your current payslip: "); 
+				payslip.printPayslip(employeeID);
 				
+			}else if(command.equals("P")) {
+				System.out.print("Here are your past payslips: "); 
+				 
+			}else if(command.equals("L")) {
+				System.out.print("Logging out...");
+			}
+			
+			else {
+				
+				System.out.print("Logging out...");
+				
+			}
+		}else {
+			System.out.println("C)urrent Payslip, P)ast payslips L)ogout "); 
+			String command = in.nextLine().toUpperCase(); 
+		
+			Payslip payslip = new Payslip(); 
+		
+			if(command.equals("C")) {
+				System.out.print("Here is your current payslip: "); 
+				payslip.printPayslip(employeeID);
+			
+			}else if(command.equals("P")) {
+				System.out.print("Past payslips are not available for part-time employees."); 
+			 
+			}else if(command.equals("L")) {
+				System.out.print("Logging out..."); 
+			}
+		
+			else {
+	
+				System.out.print("Logging out...");	
 		}
 	}
 
 }
-
+}
