8ae02d4c3d63462912732c4bd7f46ae7b4a19dea
rmcguire211 <rebeccamcg13@gmail.com>
2024-11-28 22:32:02 +0000

    Update EmployeeMenu.java

--- EmployeeMenu.java
+++ EmployeeMenu.java
@@ -6,19 +6,18 @@ public class EmployeeMenu {
 	public static void showMenu(int employeeID) {
 		Scanner in = new Scanner(System.in);
 		System.out.println("Select option applies to you: ");
-		System.out.println("I am a F)ull-time employye\nI am as P)art-time employee L)ogout "); 
+		System.out.println("I am a F)ull-time employye\nI am as P)art-time employee \nL)ogout "); 
 		String chosen = in.nextLine().toUpperCase(); 
 		
 		if(chosen.equals("F")) {
-			System.out.println("Select option applies to you: ");
-			System.out.println("C)urrent Payslip\n P)ast payslips L)ogout "); 
+			System.out.println("Select what payslip you would like to view: ");
+			System.out.println("C)urrent Payslip \nP)ast payslips \nL)ogout "); 
 			String command = in.nextLine().toUpperCase(); 
-			
 			Payslip payslip = new Payslip(); 
 			
 			if(command.equals("C")) {
 				System.out.print("Here is your current payslip: "); 
-				payslip.printPayslip(employeeID);
+				payslip.printPayslipAtIndex(0);
 				
 			}else if(command.equals("P")) {
 				System.out.print("Here are your past payslips: "); 
@@ -35,12 +34,11 @@ public class EmployeeMenu {
 		}else {
 			System.out.println("C)urrent Payslip, P)ast payslips L)ogout "); 
 			String command = in.nextLine().toUpperCase(); 
-		
-			Payslip payslip = new Payslip(); 
+			 
 		
 			if(command.equals("C")) {
 				System.out.print("Here is your current payslip: "); 
-				payslip.printPayslip(employeeID);
+				
 			
 			}else if(command.equals("P")) {
 				System.out.print("Past payslips are not available for part-time employees."); 
