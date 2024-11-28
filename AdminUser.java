a80f06ec3a789371c8204d430be7a5b8949118f2
rmcguire211 <rebeccamcg13@gmail.com>
2024-11-28 20:38:00 +0000

    Create AdminUser.java

---
+++ AdminUser.java
@@ -0,0 +1,24 @@
+package Project;
+
+public class AdminUser{
+	private static String adminID = "1234";
+	private static String password = "admin_pass";
+
+	public AdminUser(String AdminID, String password) {
+		this.adminID = AdminID;
+		this.password = password;
+	}
+	
+	public static String getAdminID() {
+		return adminID;
+	}
+	
+	public static String getPassword() {
+		return password;
+	}
+	
+	public boolean validatePassword(String inputPassword) {
+		return this.password.equals(inputPassword);
+	}
+
+}
