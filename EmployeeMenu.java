3b9e3046abd5d3fae519f0a8669e3c7a82477be3
LauraFlanagan <20259085@studentmail.ul.ie>
2024-11-28 20:56:07 +0000

    Update EmployeeMenu.java

--- EmployeeMenu.java
+++ EmployeeMenu.java
@@ -4,18 +4,23 @@ public class EmployeeMenu {
 	public static void showMenu(int employeeID) {
 		Scanner in = new Scanner(System.in);
 		System.out.print("Select option: ");
-		System.out.println("C)urrent Payslip, P)ast payslips , L)ogout "); 
+		System.out.println("C)urrent Payslip, P)ast payslips , G)et Index of Employee L)ogout "); 
 		String command = in.nextLine().toUpperCase(); 
 		
 		Payslip payslip = new Payslip(); 
 		
 		if(command.equals("C")) {
+			
 			payslip.printPayslip(employeeID);
 			
 		}else if(command.equals("P")) {
 			System.out.print("Here are your past payslips"); 
 			 
-		}else {
+		}else if(command.equals("G")) {
+			System.out.print(Employees.getIndexOfEmployeeID(employeeID)); 
+		}
+		
+		else {
 			
 			System.out.print("Logging out...");
 				
@@ -23,3 +28,4 @@ public class EmployeeMenu {
 	}
 
 }
+
