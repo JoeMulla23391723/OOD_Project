eaa226e7241c85211b10eceb97fda3990a31ec4c
JoeMulla23391723 <23391723@studentmail.ul.ie>
2024-11-30 13:15:23 +0000

    Update Payslip.java

--- Payslip.java
+++ Payslip.java
@@ -12,32 +12,32 @@ public class Payslip {
     //method that should see if an employee is either part-time or full-time, then prints the relevant payslip.
     public void printPayslip(int employeeID, int index) {
         Employee employee = Employees.getEmployeeFromIndex(employeeID);
-        PartTimeDeductionsCalculator partTimeCalculator = new PartTimeDeductionsCalculator();
-        FullTimeDeductionsCalculator fullTimeCalculator = new FullTimeDeductionsCalculator();
+        PartTimeSalaryCalculator partTimeCalculator = new PartTimeSalaryCalculator();
+        FullTimeSalaryCalculator fullTimeCalculator = new FullTimeSalaryCalculator();
         if (employee instanceof PartTimeEmployee) { //part-time payslips only show current months based on one hoursWorked value
             PartTimeEmployee partTimeEmployee = (PartTimeEmployee) employee;
             if(partTimeEmployee.validSubmissionDate(partTimeEmployee.getDatePayClaimSubmitted())){
-            System.out.println("--------------------------------------------------------");
-            System.out.println("                     Payslip                           ");
-            System.out.println("--------------------------------------------------------");
-            System.out.println("Employee Name: " + partTimeEmployee.getName());
-            System.out.println("Employee ID:   " + partTimeEmployee.getId());
-            System.out.println(("Employee PPS: " + partTimeEmployee.getPps()));
-            System.out.println("Date:          " + "25/" + month + "/2024");
-            System.out.println("--------------------------------------------------------");
-            System.out.println("Hours Worked:        " + partTimeEmployee.getHoursWorkedThisPayPeriod());
-            System.out.println("Gross Pay:           €" + (partTimeEmployee.getHoursWorkedThisPayPeriod() * partTimeEmployee.getHourlyRate()));
-            System.out.println("--------------------------------------------------------");
-            System.out.println("Deductions:");
-            System.out.printf("    USC:             €%.2f%n", partTimeCalculator.calculateUscPaid(employeeID));
-            System.out.printf("    PRSI:            €%.2f%n", partTimeCalculator.calculatePrsiPaid(employeeID));
-            System.out.printf("    Union Fees:      €%.2f%n", partTimeCalculator.calculateUnionFees(employeeID));
-            System.out.printf("    Health Insurance:€%.2f%n", partTimeCalculator.calculateHeathInsuranceCost(employeeID));
-            System.out.printf("    Net Tax:         €%.2f%n", partTimeCalculator.calculateNetTax(employeeID));
-            System.out.println("--------------------------------------------------------");
-            System.out.printf("PRSI Subclass:       %s%n", partTimeCalculator.getPrsiSubClass());
-            System.out.printf("Income Pay:            €%.2f%n", partTimeCalculator.calculateNetPay(employeeID));
-            System.out.println("--------------------------------------------------------");
+                System.out.println("--------------------------------------------------------");
+                System.out.println("                     Payslip                           ");
+                System.out.println("--------------------------------------------------------");
+                System.out.println("Employee Name: " + partTimeEmployee.getName());
+                System.out.println("Employee ID:   " + partTimeEmployee.getId());
+                System.out.println(("Employee PPS:  " + partTimeEmployee.getPps()));
+                System.out.println("Date:          " + "25/" + month + "/2024");
+                System.out.println("--------------------------------------------------------");
+                System.out.println("Hours Worked:        " + partTimeEmployee.getHoursWorkedThisPayPeriod());
+                System.out.println("Gross Pay:           €" + (partTimeEmployee.getHoursWorkedThisPayPeriod() * partTimeEmployee.getHourlyRate()));
+                System.out.println("--------------------------------------------------------");
+                System.out.println("Deductions:");
+                System.out.printf("    USC:             €%.2f%n", partTimeCalculator.calculateUscPaid(employeeID));
+                System.out.printf("    PRSI:            €%.2f%n", partTimeCalculator.calculatePrsiPaid(employeeID));
+                System.out.printf("    Union Fees:      €%.2f%n", partTimeCalculator.calculateUnionFees(employeeID));
+                System.out.printf("    Health Insurance:€%.2f%n", partTimeCalculator.calculateHeathInsuranceCost(employeeID));
+                System.out.printf("    Income Tax:         €%.2f%n", partTimeCalculator.calculateNetTax(employeeID));
+                System.out.println("--------------------------------------------------------");
+                System.out.printf("PRSI Subclass:       %s%n", partTimeCalculator.getPrsiSubClass());
+                System.out.printf("Net Pay:            €%.2f%n", partTimeCalculator.calculateNetPay(employeeID));
+                System.out.println("--------------------------------------------------------");
             } else if (!partTimeEmployee.validSubmissionDate(partTimeEmployee.getDatePayClaimSubmitted())) {
                 System.out.println("No Payslip Available: Payslip Submitted Too Late");
             }
@@ -62,11 +62,11 @@ public class Payslip {
                         fullTimeCalculator.getPrsiSubClass()
                 );
                 payslipArrayList.add(payslip); //can later bring up a specific payslip, doesn't show it on method call.
+                System.out.println(payslipArrayList.get(index));
             }
-        }else{
+        }   else{
             System.out.println("No payslip to be printed");
         }
-        System.out.println(payslipArrayList.get(index));
 
     }
 
